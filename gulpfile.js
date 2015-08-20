dbb5b07648a4d3a7874fb0a3589eba33ae8347a6
vbislife04mb <kalibrayfield@gmail.com>
2015-08-19 23:15:54 -0500

    navigation on project pages and small grid system

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,16 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass');
 
-gulp.task('watchSass', function() {
-	gulp.watch('scss/**/*.scss', ['compileSass']);
-});
-
 gulp.task("compileSass",function(){
 	gulp.src("scss/application.scss")
 		.pipe(sass())
 		.pipe(gulp.dest('css'));
 })
 
+gulp.task('watchSass', function() {
+	gulp.watch('scss/**/*.scss', ['compileSass']);
+})
+
 gulp.task('build',['compileSass']);
+
+gulp.task("default", ["build"]);
